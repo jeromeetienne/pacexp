c7a883eda8b37d586ab66b6e65baf7590c913f4b
Jerome Etienne <jerome.etienne@gmail.com>
2011-05-02 14:54:11 +0200

    www/js/Threex/Threex.WindowResize.js: experimentation to get font-size from windowW

--- www/js/Threex/Threex.WindowResize.js
+++ www/js/Threex/Threex.WindowResize.js
@@ -1,10 +1,25 @@
 if(typeof THREEx === "undefined")	var THREEx	= {};
 
+fontSizeAdjust	= function(){
+	var fontRatio	= 1;
+	var windowW	= 1680;
+	var ratioW	= window.innerWidth/windowW;
+	var fixRatio	= function(ratioW){ return 3*(1-ratioW) + 1; };
+	var fontSize	= 100 * fontRatio * ratioW * fixRatio(ratioW);
+	fontSize	= Math.round(fontSize*100) / 100
+	jQuery("body").css('font-size', fontSize+'%');	
+}
+
+jQuery(function(){ fontSizeAdjust() });
+
 /**
  * Adjust the display when the window is resized
 */
 THREEx.WindowResize	= function(renderer, camera){
 	var callback	= function(){
+		
+		fontSizeAdjust();
+		
 		renderer.setSize( window.innerWidth, window.innerHeight );
 		camera.aspect	= window.innerWidth / window.innerHeight;
 		camera.updateProjectionMatrix()
