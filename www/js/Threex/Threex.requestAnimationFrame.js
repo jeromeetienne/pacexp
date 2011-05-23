040ec1f47e0c041ff603189a5536a39f887606b8
Jerome Etienne <jerome.etienne@gmail.com>
2011-05-23 16:42:15 +0200

    www/vendor/three.js: fix a bit the path

---
+++ www/js/Threex/Threex.requestAnimationFrame.js
@@ -0,0 +1,36 @@
+/**
+ * Provides requestAnimationFrame in a cross browser way.
+ * http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+ */
+
+if ( !window.requestAnimationFrame ) {
+
+	window.requestAnimationFrame = ( function() {
+
+		return window.webkitRequestAnimationFrame ||
+		window.mozRequestAnimationFrame ||
+		window.oRequestAnimationFrame ||
+		window.msRequestAnimationFrame ||
+		function( /* function FrameRequestCallback */ callback, /* DOMElement Element */ element ) {
+
+			return window.setTimeout( callback, 1000 / 60 );
+
+		};
+
+	} )();
+
+}
+
+if ( !window.cancelRequestAnimationFrame ) {
+
+	window.cancelRequestAnimationFrame = ( function() {
+
+		return window.webkitCancelRequestAnimationFrame ||
+		window.mozCancelRequestAnimationFrame ||
+		window.oCancelRequestAnimationFrame ||
+		window.msCancelRequestAnimationFrame ||
+		clearTimeout
+
+	} )();
+
+}
